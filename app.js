2724b4268e8023662302e6532ad7074c8201d271
silvia <silvia.pintus@tipser.com>
2021-09-04 18:06:31 +0000

    replace body parser

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
